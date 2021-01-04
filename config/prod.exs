114d392e6157f8975a5d98cd74dbc2df82103362
Al Dee <amyalicedee@gmail.com>
2021-01-04 18:21:45 +0000

    Move hostname config to runtime

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,6 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kalda, KaldaWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
