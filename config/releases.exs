e59a654d47d109c078bdc02163cbea80586ca49a
Al Dee <amyalicedee@gmail.com>
2021-01-04 17:39:29 +0000

    Config updated for deploy to Render

--- config/releases.exs
+++ config/releases.exs
@@ -1,8 +1,4 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -28,14 +24,5 @@ config :kalda, KaldaWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
-
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :kalda, KaldaWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
+  secret_key_base: secret_key_base,
+  server: true
