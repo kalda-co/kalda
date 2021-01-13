892c01d6c3f69777e88706a8a1556dc5fa04c30a
Al Dee <amyalicedee@gmail.com>
2021-01-13 16:01:35 +0000

    Adds seeds and IO printing username and password for dev

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Kalda.Accounts.User
+
+_user =
+  Kalda.Repo.insert!(%User{
+    email: "demo@kalda.co",
+    password: "thisisademopassword",
+    hashed_password: Bcrypt.hash_pwd_salt("thisisademopassword")
+  })
+
+_user2 =
+  Kalda.Repo.insert!(%User{
+    email: "user@example.com",
+    password: "thisisatestpassword",
+    hashed_password: Bcrypt.hash_pwd_salt("thisisatestpassword")
+  })
+
+IO.puts("""
+You can now log in with these credentials:
+email:    demo@kalda.co
+password: thisisademopassword
+""")
