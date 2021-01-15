d377e97f0368007d5880b9c7d9f903cbdbb2a2c8
Louis Pilfold <louis@lpil.uk>
2021-01-15 17:34:58 +0000

    Remove duplicate pipeline, landing -> site

--- lib/kalda_web/controllers/blog_controller.ex
+++ lib/kalda_web/controllers/blog_controller.ex
@@ -1,6 +1,8 @@
 defmodule KaldaWeb.BlogController do
   use KaldaWeb, :controller
 
+  plug :put_root_layout, {KaldaWeb.LayoutView, :site_page}
+
   def show(conn, params) do
     post = Kalda.Blog.get_post_by_id!(params["id"])
 
--- lib/kalda_web/controllers/page_controller.ex
+++ lib/kalda_web/controllers/page_controller.ex
@@ -1,6 +1,8 @@
 defmodule KaldaWeb.PageController do
   use KaldaWeb, :controller
 
+  plug :put_root_layout, {KaldaWeb.LayoutView, :site_page}
+
   def index(conn, _params) do
     conn.assigns.current_user
 
