c7c82d7c5f4cf8376c558ad8319a4d3e206a33bd
Al Dee <amyalicedee@gmail.com>
2021-01-17 12:22:41 +0000

    Test for  route in PageController

--- test/kalda_web/controllers/page_controller_test.exs
+++ test/kalda_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule KaldaWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "kalda"
   end
+
+  test "Get /thanks", %{conn: conn} do
+    conn = get(conn, "/thanks")
+    assert html_response(conn, 200) =~ "Thank"
+  end
 end
