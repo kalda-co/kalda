e9e09574b698f290f3c2833f5f00be18fbadf1e2
Al Dee <amyalicedee@gmail.com>
2021-01-18 07:47:36 +0000

    Debug

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,3 +39,22 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+const registerForm = document.querySelector(".js-waitlist-form");
+
+if (registerForm) {
+  registerForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    setTimeout(submitForm, 1000);
+    var formSubmitted = false;
+
+    function submitForm() {
+      if (!formSubmitted) {
+        formSubmitted = true;
+        registerForm.submit();
+      }
+    }
+
+    plausible('waitlist', { callback: submitForm });
+  })
+
+}
--- assets/js/plausible.js
+++ assets/js/plausible.js
@@ -1,22 +1,22 @@
-const registerForm = document.querySelector(".js-waitlist-form");
+// const registerForm = document.querySelector(".js-waitlist-form");
 
-if (registerForm) {
-  registerForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    setTimeout(submitForm, 1000);
-    var formSubmitted = false;
+// if (registerForm) {
+//   registerForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     setTimeout(submitForm, 1000);
+//     var formSubmitted = false;
 
-    function submitForm() {
-      if (!formSubmitted) {
-        formSubmitted = true;
-        registerForm.submit();
-      }
-    }
+//     function submitForm() {
+//       if (!formSubmitted) {
+//         formSubmitted = true;
+//         registerForm.submit();
+//       }
+//     }
 
-    plausible('waitlist', { callback: submitForm });
-  })
+//     plausible('waitlist', { callback: submitForm });
+//   })
 
-}
+// }
 
-// TODO Add a logSubmit event ? see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
-// TODO Add custom props such as OS and device, see https://plausible.io/docs/custom-event-goals
+// // TODO Add a logSubmit event ? see https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+// // TODO Add custom props such as OS and device, see https://plausible.io/docs/custom-event-goals
