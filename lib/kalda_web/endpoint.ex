031e0417b43da75782a985b77856e272c954a865
Al Dee <amyalicedee@gmail.com>
2021-01-18 07:23:03 +0000

    Set gzip to true for digesting static files in prod

--- lib/kalda_web/endpoint.ex
+++ lib/kalda_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule KaldaWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :kalda,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
