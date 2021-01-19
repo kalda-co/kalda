b31e9f94b27d34369e452baabf0cf54b48926ea9
Al Dee <amyalicedee@gmail.com>
2021-01-19 08:28:17 +0000

    Add PostLive route(index), controller, tmplate and tests

---
+++ lib/kalda_web/live/post_live/index.ex
@@ -0,0 +1,29 @@
+defmodule KaldaWeb.PostLive.Index do
+  use KaldaWeb, :live_view
+
+  alias Kalda.Forums
+  alias Kalda.Forums.Post
+
+  @impl true
+  def mount(_params, _session, socket) do
+    # {:ok, assign(socket, :posts, get_posts())}
+    {:ok,
+     socket
+     |> assign(posts: get_posts())}
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Geting Posts")
+    |> assign(:post, nil)
+  end
+
+  defp get_posts do
+    Forums.get_posts()
+  end
+end
