095f453220f5c2a01ab73f06d318461cc40cd907
Al Dee <amyalicedee@gmail.com>
2021-01-20 08:09:54 +0000

    Fix Forums Post tests; add user to post

--- test/kalda/forums_test.exs
+++ test/kalda/forums_test.exs
@@ -2,6 +2,7 @@ defmodule Kalda.ForumsTest do
   use Kalda.DataCase
 
   alias Kalda.Forums
+  alias Kalda.AccountsFixtures
 
   describe "posts" do
     alias Kalda.Forums.Post
@@ -20,43 +21,55 @@ defmodule Kalda.ForumsTest do
     end
 
     test "create_post/1 with valid attrs" do
-      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs)
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
       assert post.content == "some content"
     end
 
     test "create_post/1 with invalid attrs" do
-      assert {:error, %Ecto.Changeset{}} = Forums.create_post(@invalid_post_attrs)
+      user = AccountsFixtures.user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Forums.create_post(@invalid_post_attrs, user)
     end
 
     test "get_posts/0 returns all posts" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
       assert Forums.get_posts() == [post]
     end
 
     test "change_post/1 returns a post changeset" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
       assert %Ecto.Changeset{} = Forums.change_post(post)
     end
 
     test "update_post/2 with valid data updates the post" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
+
       assert {:ok, %Post{} = post} = Forums.update_post(post, @update_post_attrs)
       assert post.content == "some updated content"
     end
 
     test "update_post/2 with invalid data returns error changeset" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
+
       assert {:error, %Ecto.Changeset{}} = Forums.update_post(post, @invalid_post_attrs)
       assert post == Forums.get_post!(post.id)
     end
 
     test "get_post!/1 returns the post with given id" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
+
       assert Forums.get_post!(post.id) == post
     end
 
     test "delete_post/1 deletes the post" do
-      post = post_fixture()
+      user = AccountsFixtures.user_fixture()
+      assert {:ok, %Post{} = post} = Forums.create_post(@valid_post_attrs, user)
+
       assert {:ok, %Post{}} = Forums.delete_post(post)
       assert_raise Ecto.NoResultsError, fn -> Forums.get_post!(post.id) end
     end
