0003c7a75a2d1303a7900ee3682f3b45815ce6bb
Al Dee <amyalicedee@gmail.com>
2021-01-26 16:30:58 +0000

    Adds comments table, schema and seeds

---
+++ lib/kalda/forums/comment.ex
@@ -0,0 +1,27 @@
+defmodule Kalda.Forums.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "comments" do
+    field :content, :string
+    # field :author_id, :id
+    belongs_to :user, Kalda.Accounts.User,
+      foreign_key: :author_id,
+      references: :id
+
+    belongs_to :post, Kalda.Forums.Post,
+      foreign_key: :post_id,
+      references: :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content])
+    |> validate_required([:content, :author_id, :post_id])
+    |> foreign_key_constraint(:author_id)
+    |> foreign_key_constraint(:post_id)
+  end
+end
