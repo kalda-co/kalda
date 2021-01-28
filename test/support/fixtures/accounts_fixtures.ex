4234f09267ca7b869395b68b96c52bc59f3c6749
Al Dee <amyalicedee@gmail.com>
2021-01-28 15:04:53 +0000

    WIP Adds username to User fixture

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,13 @@ defmodule Kalda.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "hello world!"
+  def unique_username, do: "user#{System.unique_integer()}name"
 
   def user(attrs \\ %{}) do
     {:ok, user} =
       attrs
       |> Enum.into(%{
+        username: unique_username(),
         email: unique_user_email(),
         password: valid_user_password()
       })
