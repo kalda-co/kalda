d0c3e5a862b163926d483647fb140e21c5f3b917
Al Dee <amyalicedee@gmail.com>
2021-02-02 07:28:58 +0000

    New migration for is_admin and username on User

---
+++ priv/repo/migrations/20210202072358_users_add_is_admin_column_add_username_column.exs
@@ -0,0 +1,9 @@
+defmodule Kalda.Repo.Migrations.UsersAddIsAdminColumnAddUsernameColumn do
+  use Ecto.Migration
+
+  def change do
+  alter table(:users) do
+      add :is_admin, :boolean, null: false, default: false
+      add :username, :string, null: false
+  end
+end
