a186b3fedb89f29948b9ef26104ca10879db9545
Al Dee <amyalicedee@gmail.com>
2021-02-02 07:48:35 +0000

    Fix firmatting

--- priv/repo/migrations/20210202072358_users_add_is_admin_column_add_username_column.exs
+++ priv/repo/migrations/20210202072358_users_add_is_admin_column_add_username_column.exs
@@ -2,9 +2,9 @@ defmodule Kalda.Repo.Migrations.UsersAddIsAdminColumnAddUsernameColumn do
   use Ecto.Migration
 
   def change do
-  alter table(:users) do
-      add :is_admin, :boolean, null: false, default: false
-      add :username, :string, null: false
+    alter table(:users) do
+        add :is_admin, :boolean, null: false, default: false
+        add :username, :string, null: false
+    end
   end
-end
 end
