5ae58a45f4c74acc5b0623c456cb36cf82640299
Al Dee <amyalicedee@gmail.com>
2021-02-03 11:46:20 +0000

    Remove unused policies

--- lib/policy.ex
+++ lib/policy.ex
@@ -1,6 +1,6 @@
 defmodule Kalda.Policy do
   @moduledoc """
-  Rules engine for authorization. Used to check whether a entity has
+  Rules engine for authorization. Used to check whether an entity has
   permission to do something.
   """
   alias Kalda.Accounts.User
@@ -26,40 +26,17 @@ defmodule Kalda.Policy do
              :view_admin_pages,
              :create_post,
              :view_api_index
-             #  :view_admin_menu,
-             # :view_all_users,
-             #  :delete_comment,
-             #  :delete_reply
            ] do
     raise UnauthorizedError
   end
-
-  # def authorize!(%User{} = user, :edit_paper, %Collaboration{} = collaboration) do
-  #   if user.id != collaboration.user_id || !collaboration.can_edit do
-  #     raise UnauthorizedError
-  #   end
-  # end
-
-  # SHorthand version:
-  # def authorize!(%User{id: user_id}, :edit_paper, %Collaboration{can_edit: true, user_id: user_id}) do
-  #   :ok
-  # end
 
   def authorize!(%User{} = _user, _action, _subject) do
     raise UnauthorizedError
   end
 
-  # TODO test
-  def can?(user, action, subject) do
-    authorize!(user, action, subject)
-    true
-  rescue
-    Kalda.Policy.UnauthorizedError -> false
+  # Catches exception in production.
+  defimpl Plug.Exception, for: Kalda.Policy.UnauthorizedError do
+    def status(_exception), do: 404
+    def actions(_exception), do: []
   end
-end
-
-# Catches exception in production.
-defimpl Plug.Exception, for: Kalda.Policy.UnauthorizedError do
-  def status(_exception), do: 404
-  def actions(_exception), do: []
 end
