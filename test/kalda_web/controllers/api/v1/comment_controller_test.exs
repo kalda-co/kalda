086bf18c61015e64dff12ce32df6f485da3c7ca1
Al Dee <amyalicedee@gmail.com>
2021-02-05 14:39:56 +0000

    Tests sad path for API comment endpath

--- test/kalda_web/controllers/api/v1/comment_controller_test.exs
+++ test/kalda_web/controllers/api/v1/comment_controller_test.exs
@@ -4,13 +4,16 @@ defmodule KaldaWeb.Api.V1.CommentControllerTest do
   @valid_comment_content %{
     content: "This is a comment!"
   }
+  @invalid_comment_content %{
+    content: ""
+  }
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "unauthenticated requests" do
-    test "POST new", ctx do
+    test "POST create", ctx do
       assert ctx.conn |> post("/v1/posts/1/comments") |> json_response(401)
     end
   end
@@ -37,6 +40,16 @@ defmodule KaldaWeb.Api.V1.CommentControllerTest do
                "content" => @valid_comment_content.content,
                "author" => comment.author_id,
                "inserted_at" => NaiveDateTime.to_iso8601(comment.inserted_at)
+             }
+    end
+
+    test "renders 422 with errors for invalid attributes", %{conn: conn, user: _current_user} do
+      user = Kalda.AccountsFixtures.user()
+      post = Kalda.ForumsFixtures.post(user)
+      assert conn = post(conn, "/v1/posts/#{post.id}/comments", comment: @invalid_comment_content)
+
+      assert json_response(conn, 422) == %{
+               "errors" => %{"content" => ["can't be blank"]}
              }
     end
   end
