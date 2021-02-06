23299f61d027b972fd450640edce92e3b8ae4428
Al Dee <amyalicedee@gmail.com>
2021-02-06 17:47:30 +0000

    Adds fn to differentiate blog page from site page and apply correct header

--- test/kalda_web/views/layout_view_test.exs
+++ test/kalda_web/views/layout_view_test.exs
@@ -5,4 +5,22 @@ defmodule KaldaWeb.LayoutViewTest do
   # use functions such as safe_to_string() to convert the helper
   # result into an HTML string.
   # import Phoenix.HTML
+
+  test "blog_page?" do
+    conn = %{
+      host: "localhost",
+      method: "GET",
+      params: %{"id" => "do-i-have-to-be-lgbtqia"},
+      path_info: ["blog", "do-i-have-to-be-lgbtqia"],
+      path_params: %{"id" => "do-i-have-to-be-lgbtqia"},
+      port: 4000,
+      private: %{
+        KaldaWeb.Router => {[], %{}},
+        :phoenix_action => :show,
+        :phoenix_controller => KaldaWeb.BlogController
+      }
+    }
+
+    assert KaldaWeb.LayoutView.blog_page?(conn) == true
+  end
 end
