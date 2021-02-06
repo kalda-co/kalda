71a3fa15bb91cc9cd9a89323ff23b5a252ee2307
Al Dee <amyalicedee@gmail.com>
2021-02-06 05:43:40 +0000

    Adds Create Reply API endpoint

---
+++ lib/kalda_web/controllers/api/v1/reply_controller.ex
@@ -0,0 +1,17 @@
+defmodule KaldaWeb.Api.V1.ReplyController do
+  use KaldaWeb, :controller
+  alias Kalda.Forums
+  alias Kalda.Forums.Reply
+
+  def create(conn, %{"id" => comment_id, "reply" => reply_params}) do
+    user = conn.assigns.current_user
+    comment = Forums.get_comment!(comment_id)
+
+    with {:ok, %Reply{} = reply} <- Forums.create_reply(user, comment, reply_params) do
+      conn
+      |> put_status(201)
+      |> render("show.json", reply: reply)
+    end
+    |> KaldaWeb.Api.V1.handle_error(conn)
+  end
+end
---
+++ test/kalda_web/controllers/api/v1/reply_controller_test.exs
@@ -0,0 +1,61 @@
+defmodule KaldaWeb.Api.V1.ReplyControllerTest do
+  use KaldaWeb.ConnCase
+
+  @valid_reply_content %{
+    content: "This is a reply!"
+  }
+  @invalid_reply_content %{
+    content: ""
+  }
+
+  setup %{conn: conn} do
+    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  end
+
+  describe "unauthenticated requests" do
+    test "POST create", ctx do
+      assert ctx.conn |> post("/v1/comments/1/replies") |> json_response(401)
+    end
+  end
+
+  describe "POST create" do
+    setup [:register_and_log_in_user]
+
+    test "creates reply for user on post in daily reflections", %{
+      conn: conn,
+      user: current_user
+    } do
+      user = Kalda.AccountsFixtures.user()
+      post = Kalda.ForumsFixtures.post(user)
+      comment = Kalda.ForumsFixtures.comment(post, user)
+
+      assert conn = post(conn, "/v1/comments/#{comment.id}/replies", reply: @valid_reply_content)
+
+      assert [reply] = Kalda.Forums.get_replies()
+      assert reply.content == @valid_reply_content.content
+      assert reply.author_id == current_user.id
+      assert reply.post_id == post.id
+
+      assert json_response(conn, 201) == %{
+               "id" => reply.id,
+               "content" => @valid_reply_content.content,
+               "author" => reply.author_id,
+               "comment_id" => reply.comment_id,
+               "inserted_at" => NaiveDateTime.to_iso8601(reply.inserted_at)
+             }
+    end
+
+    test "renders 422 with errors for invalid attributes", %{conn: conn, user: _current_user} do
+      user = Kalda.AccountsFixtures.user()
+      post = Kalda.ForumsFixtures.post(user)
+      comment = Kalda.ForumsFixtures.comment(post, user)
+
+      assert conn =
+               post(conn, "/v1/comments/#{comment.id}/replies", reply: @invalid_reply_content)
+
+      assert json_response(conn, 422) == %{
+               "errors" => %{"content" => ["can't be blank"]}
+             }
+    end
+  end
+end
