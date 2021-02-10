f446a43a32ac02db4719495079bd713ee77291ed
Al Dee <amyalicedee@gmail.com>
2021-02-10 07:42:28 +0000

    Fixes error view test

--- test/kalda_web/views/error_view_test.exs
+++ test/kalda_web/views/error_view_test.exs
@@ -5,7 +5,7 @@ defmodule KaldaWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(KaldaWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(KaldaWeb.ErrorView, "404.html", []) =~ "not found"
   end
 
   test "renders 500.html" do
