564d61c54032a07a0f5781d989ddc0c9a1f839a9
Al Dee <amyalicedee@gmail.com>
2021-02-11 23:21:47 +0000

    SPA api for replies matches backend

--- lib/kalda_web/controllers/api/v1/comment_controller.ex
+++ lib/kalda_web/controllers/api/v1/comment_controller.ex
@@ -9,9 +9,11 @@ defmodule KaldaWeb.Api.V1.CommentController do
     post = Forums.get_post!(post_id)
 
     with {:ok, %Comment{} = comment} <- Forums.create_comment(user, post, comment_params) do
+      comment = comment |> Map.put(:author, user) |> Map.put(:replies, [])
+
       conn
       |> put_status(201)
-      |> render("show.json", comment: Map.put(comment, :author, user))
+      |> render("show.json", comment: comment)
     end
     |> KaldaWeb.Api.V1.handle_error(conn)
   end
