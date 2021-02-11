f46db315c59416864b91626e248770d4a23a69bf
Al Dee <amyalicedee@gmail.com>
2021-02-11 08:36:44 +0000

    User can create and save comments, with tests

--- lib/kalda_web/controllers/api/v1/comment_controller.ex
+++ lib/kalda_web/controllers/api/v1/comment_controller.ex
@@ -5,6 +5,7 @@ defmodule KaldaWeb.Api.V1.CommentController do
   alias Kalda.Forums.Comment
 
   def create(conn, %{"id" => post_id} = comment_params) do
+    IO.inspect(comment_params)
     user = conn.assigns.current_user
     post = Forums.get_post!(post_id)
 
@@ -18,6 +19,7 @@ defmodule KaldaWeb.Api.V1.CommentController do
 
   def show(conn, %{"id" => id}) do
     comment = Forums.get_comment!(id, preload: [:author, replies: [:author]])
-    render(conn, "show.json", comment: comment, author: comment.author)
+    # render(conn, "show.json", comment: comment, author: comment.author)
+    render(conn, "show.json", comment: comment)
   end
 end
