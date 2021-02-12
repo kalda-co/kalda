3349bae2aefa806e7cd87afa191a98082f9ad084
Al Dee <amyalicedee@gmail.com>
2021-02-12 15:58:54 +0000

    Removes warnings

--- lib/kalda_web/controllers/admin/reply_controller.ex
+++ lib/kalda_web/controllers/admin/reply_controller.ex
@@ -64,7 +64,7 @@ defmodule KaldaWeb.Admin.ReplyController do
   def delete(conn, %{"id" => id}) do
     Policy.authorize!(conn, :delete_admin_reply, Kalda)
     reply = Forums.get_reply!(id)
-    {:ok, reply} = Forums.delete_reply(reply)
+    {:ok, _reply} = Forums.delete_reply(reply)
 
     conn
     |> put_flash(:info, "Reply deleted successfully.")
