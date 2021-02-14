7c43468a367348baa116ad8861614c5a93ac7261
Louis Pilfold <louis@lpil.uk>
2021-02-14 12:05:41 +0000

    Type safe JSON decoding

---
+++ spa/src/backend/decode.ts
@@ -0,0 +1,65 @@
+// A module for safely decoding values from unknown types
+// (i.e. those we get from APIs)
+
+export type Decoder<T> = (x: unknown) => T;
+
+export function number(x: unknown): number {
+  if (typeof x === "number") return x;
+  throw new Error(` Expected number, got ${typeof x}`);
+}
+
+export function string(x: unknown): string {
+  if (typeof x === "string") return x;
+  throw new Error(` Expected string, got ${typeof x}`);
+}
+
+export function boolean(x: unknown): boolean {
+  if (typeof x === "boolean") return x;
+  throw new Error(` Expected boolean, got ${typeof x}`);
+}
+
+export function optional<Element>(
+  decoder: Decoder<Element>
+): Decoder<Element | undefined> {
+  return (x: unknown) => {
+    if (x === undefined || x === null) return undefined;
+    return decoder(x);
+  };
+}
+
+export function array<Element>(
+  decoder: Decoder<Element>
+): Decoder<Array<Element>> {
+  return (array: unknown) => {
+    if (!Array.isArray(array)) {
+      throw new Error(` Expected array, got ${typeof x}`);
+    }
+
+    let typeCheckElement = (element: unknown, i: number) => {
+      return indexInto(array, i.toString(), decoder);
+    };
+
+    return array.map(typeCheckElement);
+  };
+}
+
+export function field<Element>(
+  fieldName: string,
+  decoder: Decoder<Element>
+): Decoder<Element> {
+  return (object: unknown) => {
+    if (typeof object !== "object" || object === null) {
+      throw new Error("Expected object, got ${typeof x}");
+    }
+
+    return indexInto(object, fieldName, decoder);
+  };
+}
+
+function indexInto<T>(thing: object, index: string, decoder: Decoder<T>): T {
+  try {
+    return decoder((thing as any)[index]);
+  } catch (error) {
+    throw new Error(`[${index}]${error.message}`);
+  }
+}
--- spa/src/backend/index.ts
+++ spa/src/backend/index.ts
@@ -1,4 +1,5 @@
-import type { AppState, Comment } from "../state";
+import type { User, Post, Reply, AppState, Comment } from "../state";
+import { field, number, string, array } from "./decode";
 
 let CSRFToken = "";
 
@@ -13,38 +14,98 @@ function constructPostCommentEndpoint(post_id: number): string {
   return "/v1/posts/" + post_id + "/comments";
 }
 
-export async function getInitialAppState(): Promise<AppState> {
-  let resp = await fetch(DailyReflectionEndpoint, {
-    credentials: "include",
-    headers: { accept: "application/json" },
-  });
-  let json = await resp.json();
-  return {
-    posts: json.posts,
-    current_user: json.current_user,
+type Response = {
+  status: number;
+  body: unknown;
+};
+
+function assertStatus(resp: Response, expected: number) {
+  if (resp.status !== expected) {
+    throw new Error(`Unexpected HTTP status ${resp.status}: ${resp.body}`);
+  }
+}
+
+async function httpRequest(method: string, url: string, body: null | object) {
+  let request = {
+    headers: {
+      "content-type": "application/json",
+      "x-csrf-token": CSRFToken,
+      accept: "application/json",
+    },
+    method,
+    body: body ? JSON.stringify(body) : body,
   };
+  let resp = await fetch(url, request);
+  if (resp.status >= 500) {
+    let body = await resp.text();
+    throw new Error(`Internal server error ${resp.status} ${body}`);
+  }
+  return {
+    status: resp.status,
+    body: await resp.json(),
+  };
+}
+
+async function httpGet(url: string) {
+  return httpRequest("GET", url, null);
+}
+
+async function httpPost(url: string, body: object) {
+  return httpRequest("POST", url, body);
+}
+
+export async function getInitialAppState(): Promise<AppState> {
+  let resp = await httpGet(DailyReflectionEndpoint);
+  assertStatus(resp, 200);
+  return appState(resp.body);
 }
 
 export async function createComment(
   post_id: number,
   content: string
 ): Promise<Comment> {
-  let body = JSON.stringify({
-    content: content,
-  });
-  let resp = await fetch(constructPostCommentEndpoint(post_id), {
-    headers: {
-      "content-type": "application/json",
-      accept: "application/json",
-      "x-csrf-token": CSRFToken,
-    },
-    credentials: "include",
-    method: "POST",
-    body,
-  });
-  let json = await resp.json();
-  if (resp.status !== 201) {
-    throw new Error(`Comment could not be created. ${resp.status} ${json}`);
-  }
-  return json;
+  let url = constructPostCommentEndpoint(post_id);
+  let resp = await httpPost(url, { content });
+  assertStatus(resp, 201);
+  return comment(resp.body);
+}
+
+function appState(json: unknown): AppState {
+  return {
+    currentUser: field("current_user", user)(json),
+    posts: field("posts", array(post))(json),
+  };
+}
+
+function post(json: unknown): Post {
+  return {
+    id: field("id", number)(json),
+    content: field("content", string)(json),
+    author: field("author", user)(json),
+    comments: field("comments", array(comment))(json),
+  };
+}
+
+function comment(json: unknown): Comment {
+  return {
+    id: field("id", number)(json),
+    content: field("content", string)(json),
+    author: field("author", user)(json),
+    replies: field("replies", array(reply))(json),
+  };
+}
+
+function reply(json: unknown): Reply {
+  return {
+    id: field("id", number)(json),
+    content: field("content", string)(json),
+    author: field("author", user)(json),
+  };
+}
+
+function user(json: unknown): User {
+  return {
+    id: field("id", number)(json),
+    username: field("username", string)(json),
+  };
 }
