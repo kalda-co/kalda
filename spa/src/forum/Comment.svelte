c7a98fe3e2be81b96765ddd0c25570438edbddec
Louis Pilfold <louis@lpil.uk>
2021-02-16 17:11:17 +0000

    Return of the reply line

--- spa/src/forum/Comment.svelte
+++ spa/src/forum/Comment.svelte
@@ -16,30 +16,20 @@
     comment.replies = [...comment.replies, reply];
     replying = false;
   }
+
+  let replyLine: boolean;
+  $: {
+    replyLine = replying || comment.replies.length > 0;
+  }
 </script>
 
 <article>
-  <!-- TODO: reply line -->
-
-  <!-- <div
-    class="comment"
-    class:reply-line={!reporting && (replying || comment.replies.length > 0)}
-    class:reporting
-    transition:scale|local
-  >
-    <cite>{comment.author.username}</cite>
-    {comment.content}
-    <div class="link-container">
-      <button on:click|preventDefault={() => (replying = !replying)}
-        >Reply</button
-      >
-      <button on:click|preventDefault={() => (reporting = !reporting)}
-        >Report</button
-      >
-    </div>
-  </div> -->
-
-  <ContentBubble item={comment} report={reportComment} reply={toggleReplying} />
+  <ContentBubble
+    item={comment}
+    report={reportComment}
+    reply={toggleReplying}
+    {replyLine}
+  />
 
   <div class="replies">
     {#each comment.replies as reply (reply.id)}
@@ -63,19 +53,5 @@
   .replies {
     margin-left: var(--gap-l);
     z-index: 1; /* Bring above curvy line from comment */
-  }
-
-  .reply-line::after {
-    --border: 2px solid var(--color-grey);
-    content: "";
-    position: absolute;
-    border-left: var(--border);
-    border-bottom: var(--border);
-    border-radius: 20px;
-    bottom: calc(-1 * var(--gap-l) - var(--gap-s));
-    left: 0;
-    top: 0;
-    right: 0;
-    pointer-events: none;
   }
 </style>
