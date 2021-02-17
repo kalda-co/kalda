cbb8e556cb738feeb41b76b81631afe1c37edfcc
Al Dee <amyalicedee@gmail.com>
2021-02-17 06:51:27 +0000

    Formatted

--- priv/repo/migrations/20210127062739_create_replies.exs
+++ priv/repo/migrations/20210127062739_create_replies.exs
@@ -5,7 +5,10 @@ defmodule Kalda.Repo.Migrations.CreateReplies do
     create table(:replies) do
       add :content, :string
       add :author_id, references(:users, on_delete: :nilify_all), foreign_key: true, null: false
-      add :comment_id, references(:comments, on_delete: :delete_all), foreign_key: true, null: false
+
+      add :comment_id, references(:comments, on_delete: :delete_all),
+        foreign_key: true,
+        null: false
 
       timestamps()
     end
