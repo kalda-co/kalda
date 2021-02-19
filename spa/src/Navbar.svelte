d0077677a7c3cc18d9239785d57c0d8685df8eff
Al Dee <amyalicedee@gmail.com>
2021-02-19 13:18:37 +0000

    Adds menu styles and transition

--- spa/src/Navbar.svelte
+++ spa/src/Navbar.svelte
@@ -7,27 +7,71 @@
   import { fly } from "svelte/transition";
 </script>
 
-<div transition:fly|local class:menu>
-  {#if menu}
-    <section transition:fly={{ y: 200, duration: 1000 }}>stuff</section>
-  {/if}
-
-  <nav class="navbar">
-    <img
-      src="/images/kalda-rainbow-purple-logo.svg"
-      alt="Kalda's Rainbow Logo"
-      class="logo"
-    />
-</div>
-    <h1>Daily reflection</h1>
-    <button on:click|preventDefault={toggleMenu}>
+<nav class="nav-container">
+  <div class="navtop">
+    <div class="navbar">
       <img
-        src="/images/burger-menu.svg"
-        alt="hamburger-menu"
-        class="hamburger"
+        src="/images/kalda-rainbow-purple-logo.svg"
+        alt="Kalda's Rainbow Logo"
+        class="logo"
       />
-    </button>
-  </nav>
+      <h1>Daily reflection</h1>
+      <button on:click|preventDefault={toggleMenu}>
+        <img
+          src="/images/burger-menu.svg"
+          alt="hamburger-menu"
+          class="hamburger"
+        />
+      </button>
+    </div>
+  </div>
+
+  <div class="sidebar">
+    <div transition:fly|local class:menu>
+      {#if menu}
+        <section transition:fly={{ y: 200, duration: 1000 }}>
+          <button on:click|preventDefault={toggleMenu}>
+            <img
+              src="/images/cross.svg"
+              alt="close menu cross"
+              class="cross-top"
+            />
+          </button>
+          <div class="button-container">
+            <div class="button-grid">
+              <a href="/app">
+                <button on:click|preventDefault class="button"> Home </button>
+              </a>
+              <a href="/app">
+                <button on:click|preventDefault class="button">
+                  Group Therapy
+                </button>
+              </a>
+              <a href="/app">
+                <button on:click|preventDefault class="button">
+                  Community
+                </button>
+              </a>
+              <a href="/app">
+                <button on:click|preventDefault class="button">
+                  Urgent Support
+                </button>
+              </a>
+              <a href="/app">
+                <button on:click|preventDefault class="button">
+                  Log Out
+                </button>
+              </a>
+            </div>
+          </div>
+          <button on:click|preventDefault={toggleMenu}>
+            <img src="/images/cross.svg" alt="close menu cross" class="cross" />
+          </button>
+        </section>
+      {/if}
+    </div>
+  </div>
+</nav>
 
 <style>
   .navbar {
@@ -52,10 +96,74 @@
     height: var(--size);
     overflow: hidden;
   }
+  .nav-container {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  .navtop {
+    top: 0;
+    background-color: transparent;
+    width: 375px;
+    max-width: 100%;
+    z-index: 2;
+  }
+
+  .sidebar {
+    color: var(--color-white);
+    display: inline-block;
+    width: var(--sidebar-width);
+    position: fixed;
+    top: 0;
+    z-index: 3;
+    width: 375px;
+    pointer-events: none;
+  }
 
   section {
-    height: 90px;
     width: 100%;
     background-color: var(--color-purple);
+    display: inline-block;
+  }
+
+  .menu {
+    pointer-events: all;
+    height: 100vh;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .button-grid {
+    margin-top: 33vh;
+    margin-bottom: 33vh;
+    width: 375px;
+    height: 34vh;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+
+  .button {
+    background-color: var(--color-white);
+    color: var(--color-purple);
+    margin: var(--gap);
+    text-align: center;
+    white-space: nowrap;
+  }
+
+  .cross {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin: var(--gap-s);
+  }
+  .cross-top {
+    top: 0;
+    right: 0;
+    margin: var(--gap-s);
+    position: absolute;
   }
 </style>
