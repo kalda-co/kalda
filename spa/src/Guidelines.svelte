0e2c18e1c43230e2803c8bb806fbb66a8726df13
Al Dee <amyalicedee@gmail.com>
2021-02-20 04:21:46 +0000

    Adds SPA Guidelines and styles

---
+++ spa/src/Guidelines.svelte
@@ -0,0 +1,77 @@
+<article>
+  <section class="guideline">
+    <p>
+      We have 3 guidelines that we ask members to accept. Anyone who doesn’t
+      respect will be removed from the platform.
+    </p>
+    <div class="guideline-card">
+      <div class="card-image">
+        <div class="card-image">
+          <img
+            src="/images/waves.png"
+            alt="Photograph of waves on a beach, from above"
+          />
+        </div>
+      </div>
+      <div class="card-text">
+        <h1>No hateful language</h1>
+        <p>
+          We will not tolerate any racist, sexist, homophobic, transphobic or
+          other hate-speech or prejudiced or offensive language or behaviour
+          within this group.
+        </p>
+      </div>
+    </div>
+    <div class="guideline-card">
+      <div class="card-image">
+        <img
+          src="/images/beach.png"
+          alt="Photograph of a person walking on sand"
+        />
+      </div>
+      <div class="card-text">
+        <h1>Confidentiality</h1>
+        <p>
+          Keep what is shared and discussed within the group confidential, don’t
+          share it outside of the group. Keep the identities of members of the
+          group private.
+        </p>
+      </div>
+    </div>
+    <div class="guideline-card">
+      <div class="card-image">
+        <img
+          src="/images/starfish.png"
+          alt="A photograph of a Starfish on the ocean floor"
+        />
+      </div>
+      <div class="card-text">
+        <h1>Safety</h1>
+        <p>
+          Kalda is not a crisis service. If you feel unable to keep yourself
+          safe please click on the ‘Urgent Support’ button to access crisis
+          services. If you feel someone else might be at risk of suicide inform
+          one of our friendly Kalda moderators by flagging the post rather than
+          providing support to them yourself.
+        </p>
+      </div>
+    </div>
+  </section>
+</article>
+
+<style>
+  .guideline {
+    padding: var(--gap);
+  }
+
+  .card-text {
+    --border: 2px solid var(--color-grey);
+    background-color: var(--color-grey);
+    padding: var(--gap);
+    margin-top: calc(0px - var(--gap));
+    border-left: var(--border);
+    border-bottom: var(--border);
+    border-radius: 20px;
+    margin-bottom: var(--gap-s);
+  }
+</style>
