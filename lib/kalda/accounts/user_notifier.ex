09b037ab0d49c9fbcb47dce60c3ee9d9611705a1
Al Dee <amyalicedee@gmail.com>
2021-02-20 03:11:35 +0000

    Fix config for sendgrid in prod

--- lib/kalda/accounts/user_notifier.ex
+++ lib/kalda/accounts/user_notifier.ex
@@ -6,18 +6,6 @@ defmodule Kalda.Accounts.UserNotifier do
     |> from("hello@kalda.co")
   end
 
-  # For simplicity, this module simply logs messages to the terminal.
-  # You should replace it by a proper email or notification tool, such as:
-  #
-  #   * Swoosh - https://hexdocs.pm/swoosh
-  #   * Bamboo - https://hexdocs.pm/bamboo
-  #
-  # defp deliver(to, body) do
-  #   require Logger
-  #   Logger.debug(body)
-  #   {:ok, %{to: to, body: body}}
-  # end
-
   @doc """
   Deliver instructions to confirm account.
   """
