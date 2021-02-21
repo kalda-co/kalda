f92cc520049ae78607857ea061190f0d42ed09cc
Al Dee <amyalicedee@gmail.com>
2021-02-21 12:37:18 +0000

    Post has forum enum type

---
+++ priv/repo/migrations/20210221122057_posts_add_forum_enum.exs
@@ -0,0 +1,21 @@
+defmodule Kalda.Repo.Migrations.PostsAddForumEnum do
+  use Ecto.Migration
+
+  def change do
+    create_query = """
+    CREATE TYPE forum_type AS ENUM (
+      'daily_reflection',
+      'will_pool',
+      'community',
+      'co_working'
+    )
+    """
+    drop_query = "DROP TYPE forum_type"
+    execute(create_query, drop_query)
+
+    alter table(:posts) do
+      add :forum, :forum_type, default: "daily_reflection", null: false
+    end
+
+  end
+end
