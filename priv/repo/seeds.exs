7615b629be6e3570f074ac79db34f437e38f5e99
Al Dee <amyalicedee@gmail.com>
2021-02-26 08:09:58 +0000

    Updates docs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -154,15 +154,14 @@ _report22 =
 
 _therapy_session_past =
   Kalda.Repo.insert!(%TherapySession{
-   event_datetime: NaiveDateTime.new!(~D[2020-01-01], ~T[18:30:00])
-   link: "https://us02web.zoom.us/j/9367762569?pwd=cjFZc0MxczcxR3hkdTZPekRlbFkzQT09"
+    event_datetime: NaiveDateTime.new!(~D[2020-01-01], ~T[18:30:00]),
+    link: "https://us02web.zoom.us/j/9367762569?pwd=cjFZc0MxczcxR3hkdTZPekRlbFkzQT09"
   })
 
-
-_therapy_session_future=
+_therapy_session_future =
   Kalda.Repo.insert!(%TherapySession{
-   event_datetime: NaiveDateTime.new!(~D[2030-01-01], ~T[18:30:00])
-   link: "https://us02web.zoom.us/j/9367762569?pwd=cjFZc0MxczcxR3hkdTZPekRlbFkzQT09"
+    event_datetime: NaiveDateTime.new!(~D[2030-01-01], ~T[18:30:00]),
+    link: "https://us02web.zoom.us/j/9367762569?pwd=cjFZc0MxczcxR3hkdTZPekRlbFkzQT09"
   })
 
 %{token: token, changeset: invite_changeset} = Invite.build_invite("invite@example.com")
