918504f5f272e9001f1fcce6af5bcfed5b3d3bd1
Al Dee <amyalicedee@gmail.com>
2021-03-02 12:57:36 +0000

    Admin styles

--- lib/kalda_web/controllers/admin/admin_controller.ex
+++ lib/kalda_web/controllers/admin/admin_controller.ex
@@ -7,7 +7,6 @@ defmodule KaldaWeb.Admin.AdminController do
   def index(conn, _params) do
     Policy.authorize!(conn, :view_admin, Kalda)
 
-    [latest_daily_reflection] = Forums.get_forums_posts_limit(:daily_reflection, 1)
     changeset = Forums.change_daily_reflection(%Forums.Post{})
 
     reports =
@@ -21,7 +20,6 @@ defmodule KaldaWeb.Admin.AdminController do
       )
 
     render(conn, "index.html",
-      daily_reflection: latest_daily_reflection,
       changeset: changeset,
       reports: reports
     )
