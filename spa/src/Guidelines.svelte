46e7975325c8dd65b6882582b21f1381d967e65d
Louis Pilfold <louis@lpil.uk>
2021-03-09 20:34:56 +0000

    Guidelines page is responsive

--- spa/src/Guidelines.svelte
+++ spa/src/Guidelines.svelte
@@ -1,76 +1,77 @@
-<article>
-  <section class="guideline">
-    <p>
-      We have 3 guidelines that we ask members to accept. Anyone who doesn’t
-      respect these will be removed from the platform.
-    </p>
-    <div class="guideline-card">
-      <div class="card-image">
-        <img
-          src="/images/waves.png"
-          alt="Photograph of waves on a beach, from above"
-        />
-      </div>
-      <div class="card-text">
-        <h1>No hateful language</h1>
-        <p>
-          We will not tolerate any racist, sexist, homophobic, transphobic or
-          other hate-speech or prejudiced or offensive language or behaviour
-          within this group.
-        </p>
-      </div>
+<article class="content">
+  <p>
+    We have 3 guidelines that we ask members to accept. Anyone who doesn’t
+    respect these will be removed from the platform.
+  </p>
+
+  <div class="card">
+    <div class="card-image wave" />
+    <div class="card-text">
+      <h1>No hateful language</h1>
+      <p>
+        We will not tolerate any racist, sexist, homophobic, transphobic or
+        other hate-speech or prejudiced or offensive language or behaviour
+        within this group.
+      </p>
     </div>
-    <div class="guideline-card">
-      <div class="card-image">
-        <img
-          src="/images/beach.png"
-          alt="Photograph of a person walking on sand"
-        />
-      </div>
-      <div class="card-text">
-        <h1>Confidentiality</h1>
-        <p>
-          Keep what is shared and discussed within the group confidential, don’t
-          share it outside of the group. Keep the identities of members of the
-          group private.
-        </p>
-      </div>
+  </div>
+
+  <div class="card">
+    <div class="card-image beach" />
+    <div class="card-text">
+      <h1>Confidentiality</h1>
+      <p>
+        Keep what is shared and discussed within the group confidential, don’t
+        share it outside of the group. Keep the identities of members of the
+        group private.
+      </p>
     </div>
-    <div class="guideline-card">
-      <div class="card-image">
-        <img
-          src="/images/starfish.png"
-          alt="A photograph of a Starfish on the ocean floor"
-        />
-      </div>
-      <div class="card-text">
-        <h1>Safety</h1>
-        <p>
-          Kalda is not a crisis service. If you feel unable to keep yourself
-          safe please click on the ‘Urgent Support’ button to access crisis
-          services. If you feel someone else might be at risk of suicide inform
-          one of our friendly Kalda moderators by flagging the post rather than
-          providing support to them yourself.
-        </p>
-      </div>
+  </div>
+
+  <div class="card">
+    <div class="card-image starfish" />
+    <div class="card-text">
+      <h1>Safety</h1>
+      <p>
+        Kalda is not a crisis service. If you feel unable to keep yourself safe
+        please click on the ‘Urgent Support’ button to access crisis services.
+        If you feel someone else might be at risk of suicide inform one of our
+        friendly Kalda moderators by flagging the post rather than providing
+        support to them yourself.
+      </p>
     </div>
-  </section>
+  </div>
 </article>
 
 <style>
-  .guideline {
-    padding: var(--gap);
+  .card {
+    margin-bottom: var(--gap);
+  }
+
+  .card-image {
+    height: 250px;
+    border-radius: 20px 20px 0 0;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .wave {
+    background-image: url("/images/waves.png");
+  }
+  .beach {
+    background-image: url("/images/beach.png");
+  }
+  .starfish {
+    background-image: url("/images/starfish.png");
   }
 
   .card-text {
-    --border: 2px solid var(--color-grey);
     background-color: var(--color-grey);
     padding: var(--gap);
-    margin-top: calc(0px - var(--gap));
-    border-left: var(--border);
-    border-bottom: var(--border);
-    border-bottom-left-radius: 20px;
-    border-bottom-right-radius: 20px;
-    margin-bottom: var(--gap-s);
+    border-radius: 0 0 20px 20px;
+  }
+
+  h1 {
+    margin: 0 0 var(--gap) 0;
   }
 </style>
