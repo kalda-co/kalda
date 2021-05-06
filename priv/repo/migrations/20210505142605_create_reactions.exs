654bad1f30d60f5e881e895cfd9a776046139117
Al Dee <amyalicedee@gmail.com>
2021-05-06 20:04:40 +0100

    Primary key composite order corrected

--- priv/repo/migrations/20210505142605_create_reactions.exs
+++ priv/repo/migrations/20210505142605_create_reactions.exs
@@ -5,8 +5,9 @@ defmodule Kalda.Repo.Migrations.CreateReactions do
     create table(:comment_reactions, primary_key: false) do
       add :send_love, :boolean, default: false, null: false
       add :relate, :boolean, default: false, null: false
-      add :author_id, references(:users, on_delete: :nothing), foreign_key: true, null: false, primary_key: true
+      # Order is important, because of the composite primary key
       add :comment_id, references(:comments, on_delete: :nothing), foreign_key: true, null: false, primary_key: true
+      add :author_id, references(:users, on_delete: :nothing), foreign_key: true, null: false, primary_key: true
 
       timestamps()
     end
