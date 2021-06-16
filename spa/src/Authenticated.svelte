6cf16dee288ed90c87849df59e9e9c23dda2d9c3
Louis Pilfold <louis@lpil.uk>
2021-06-16 18:34:23 +0100

    Load latest state on app focus

--- spa/src/Authenticated.svelte
+++ spa/src/Authenticated.svelte
@@ -1,12 +1,44 @@
 <script lang="ts">
-  import type { ApiClient } from "./backend";
-  import { AuthenticatedApiClient } from "./backend";
   import Loaded from "./Loaded.svelte";
+  import { onDestroy } from "svelte";
+  import type { ApiClient } from "./backend";
+  import { MINUTE } from "./constants";
+  import {
+    currentNetworkConnectionStatus,
+    whenAppForegrounded,
+  } from "./device";
 
   export let api: ApiClient;
+
+  let appState = api.getInitialAppState();
+  let stateLastRefreshedAt = new Date();
+
+  async function refreshStateIfStale() {
+    let status = await currentNetworkConnectionStatus();
+    if (status === "wifi" || (status === "cellular" && enoughTimeElapsed())) {
+      refreshAppState();
+    }
+  }
+
+  async function refreshAppState() {
+    let newState = await api.getInitialAppState();
+    appState = Promise.resolve(newState);
+  }
+
+  function enoughTimeElapsed(): boolean {
+    let now = new Date().getTime();
+    let cacheInvalidationTime = stateLastRefreshedAt.getTime() + 5 * MINUTE;
+    return now > cacheInvalidationTime;
+  }
+
+  // Add an event listener so that the appState can be refreshed from the
+  // backend when the user returns to the app, assuming that enough time has
+  // passed since the previous loading of the state.
+  let foregroundListenerHandle = whenAppForegrounded(refreshStateIfStale);
+  onDestroy(() => foregroundListenerHandle.cancel());
 </script>
 
-{#await api.getInitialAppState()}
+{#await appState}
   <!-- TODO: Loading design -->
   ... Loading
 {:then state}
