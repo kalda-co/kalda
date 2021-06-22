f3df98fee8e0fa41da4a2788ce16777a95bb492e
Louis Pilfold <louis@lpil.uk>
2021-06-22 11:34:18 +0100

    Rollbar tracking on the front end
    Not sure if the native Android part is working, but our code is.

--- spa/src/index.ts
+++ spa/src/index.ts
@@ -1,6 +1,22 @@
 import App from "./App.svelte";
+import Rollbar from "rollbar";
+
+declare global {
+  interface Window {
+    Rollbar: Rollbar;
+  }
+}
 
 let apiBase: string = document.body.dataset.apiBase || "";
+let environment: string = document.body.dataset.environment || "unknown";
+
+window.Rollbar = new Rollbar({
+  accessToken: "5bf70821e7bb4dbc8c77e91549809bee",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment,
+  enabled: environment !== "dev",
+});
 
 let app = new App({
   target: document.body,
