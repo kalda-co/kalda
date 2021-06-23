ded49efaa7594e54e4f7381604e4d364fd3f1954
Al Dee <amyalicedee@gmail.com>
2021-06-23 12:54:33 +0100

    Test and notification for day and hour before group session

--- spa/src/local-notification.ts
+++ spa/src/local-notification.ts
@@ -35,8 +35,14 @@ function dailyReflection(index: number): LocalNotificationSchema {
 
 // Date.getDay() returns integer where Sunday == 0
 const WEEKLY_SESSION_REMINDER_DAY = 2; // Tuesday
+const HOURLY_SESSION_REMINDER_DAY = 3; // Wednesday
 const WEEKLY_SESSION_TIME_HOURS = 19; // 19:00pm
-const WEEKLY_SESSION_FIRST_ID = 10000;
+const HOURLY_SESSION_TIME_PROMPT = 18; // 18:00pm
+const WEEKLY_SESSION_FIRST_ID = 20000;
+const HOURLY_SESSION_FIRST_ID = 30000;
+const SESSION_NOTIFICATIONS_TO_SCHEDULE = 3; //up to 3 weeks
+const SESSION_HOUR_BODY = "Your Kalda Group Session starts in 1 hour at 7pm";
+const SESSION_WEEK_BODY = "Your kalda Group Session is tomorrow at 7pm.";
 
 var TEST_DATE: Date;
 
@@ -62,6 +68,29 @@ export function weeklySessionDayBefore(index: number): LocalNotificationSchema {
   };
 }
 
+export function weeklySessionNotification(
+  index: number,
+  notificationDay: number,
+  notificationTime: number,
+  notificationFirstID: number,
+  notificationBody: string
+): LocalNotificationSchema {
+  let date = TEST_DATE || new Date();
+  let afterReminderTime = date.getHours() >= notificationTime;
+  let effectiveDayOfWeek = date.getDay() + (afterReminderTime ? 1 : 0);
+  let reminderInDays = (7 - (effectiveDayOfWeek - notificationDay)) % 7;
+  date.setDate(date.getDate() + reminderInDays);
+  date.setHours(notificationTime);
+  date.setMinutes(0);
+  return {
+    id: notificationFirstID + index,
+    title: "Therapy Reminder",
+    // body: "Your Kalda Group Session starts in 1 hour at 7pm",
+    body: notificationBody,
+    schedule: { at: date },
+  };
+}
+
 export async function cancelDailyReflectionNotifications(): Promise<void> {
   let notifications = range(DAILY_REFLECTION_DAYS_TO_SCHEDULE).map((index) => ({
     id: DAILY_REFLECTION_FIRST_ID + index,
@@ -75,6 +104,7 @@ function range(upTo: number) {
     .map((_, index) => index);
 }
 
+<<<<<<< HEAD
 async function cancel(
   notifications: Array<LocalNotificationDescriptor>
 ): Promise<void> {
@@ -93,4 +123,41 @@ async function schedule(
   } catch (_error) {
     log.info("Local notifications not supported");
   }
+=======
+export async function cancelNotifications(
+  firstNotificationID: number,
+  numberNotificationsScheduled: number
+): Promise<void> {
+  let notifications = range(numberNotificationsScheduled).map((index) => ({
+    id: firstNotificationID + index,
+  }));
+  await LocalNotifications.cancel({ notifications });
+}
+
+export async function scheduleTherapyNotifications(): Promise<void> {
+  await cancelNotifications(
+    WEEKLY_SESSION_FIRST_ID,
+    SESSION_NOTIFICATIONS_TO_SCHEDULE
+  );
+  await cancelNotifications(
+    HOURLY_SESSION_FIRST_ID,
+    SESSION_NOTIFICATIONS_TO_SCHEDULE
+  );
+  await LocalNotifications.schedule({
+    notifications: range(SESSION_NOTIFICATIONS_TO_SCHEDULE).map(
+      weeklySessionDayBefore
+    ),
+  });
+  await LocalNotifications.schedule({
+    notifications: range(SESSION_NOTIFICATIONS_TO_SCHEDULE).map((x) =>
+      weeklySessionNotification(
+        x,
+        HOURLY_SESSION_REMINDER_DAY,
+        HOURLY_SESSION_TIME_PROMPT,
+        HOURLY_SESSION_FIRST_ID,
+        SESSION_HOUR_BODY
+      )
+    ),
+  });
+>>>>>>> 198796f (Test and notification for day and hour before group session)
 }
