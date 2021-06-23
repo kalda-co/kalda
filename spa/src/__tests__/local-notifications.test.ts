a3c893ffb47cf381f8c5418d2572e2702346ce8f
Al Dee <amyalicedee@gmail.com>
2021-06-23 12:54:33 +0100

    Test and notification for day and hour before group session

--- spa/src/__tests__/local-notifications.test.ts
+++ spa/src/__tests__/local-notifications.test.ts
@@ -1,14 +1,17 @@
 import { LocalNotifications } from "@capacitor/local-notifications";
-import { weeklySessionDayBefore } from "../local-notification";
+import {
+  weeklySessionDayBefore,
+  weeklySessionNotification,
+} from "../local-notification";
 import type { LocalNotificationSchema } from "@capacitor/local-notifications";
 import { date } from "../backend/decode";
 
-test("Day is a monday", () => {
+test("Weekly day-before notifications when today is a monday", () => {
   var TEST_DATE = new Date("June 21, 2021 18:00:00");
   let date = new Date("June 22, 2021 19:00:00");
   let notification = weeklySessionDayBefore(1);
 
-  expect(notification.id).toBe(10001);
+  expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
   let schedule = notification.schedule;
   let scheduleDate = schedule?.at;
@@ -18,12 +21,12 @@ test("Day is a monday", () => {
   expect(scheduleDate?.getHours()).toBe(19);
 });
 
-test("Day is a Tuesday, before 19:00", () => {
+test("Weekly day-before notifications when today is a Tuesday, before 19:00", () => {
   var TEST_DATE = new Date("June 22, 2021 18:00:00");
   let date = new Date("June 22, 2021 19:00:00");
   let notification = weeklySessionDayBefore(1);
 
-  expect(notification.id).toBe(10001);
+  expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
   let schedule = notification.schedule;
   let scheduleDate = schedule?.at;
@@ -33,12 +36,12 @@ test("Day is a Tuesday, before 19:00", () => {
   expect(scheduleDate?.getHours()).toBe(19);
 });
 
-test("Day is a Tuesday, after 19:00", () => {
+test("Weekly day-before notifications when today  is a Tuesday, after 19:00", () => {
   var TEST_DATE = new Date("June 15, 2021 19:01:00");
   let date = new Date("June 22, 2021 19:00:00");
   let notification = weeklySessionDayBefore(1);
 
-  expect(notification.id).toBe(10001);
+  expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
   let schedule = notification.schedule;
   let scheduleDate = schedule?.at;
@@ -48,12 +51,12 @@ test("Day is a Tuesday, after 19:00", () => {
   expect(scheduleDate?.getHours()).toBe(19);
 });
 
-test("Day is a Friday", () => {
+test("Weekly day-before notifications when today  is a Friday", () => {
   var TEST_DATE = new Date("June 18, 2021 19:01:00");
   let date = new Date("June 22, 2021 19:00:00");
   let notification = weeklySessionDayBefore(1);
 
-  expect(notification.id).toBe(10001);
+  expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
   let schedule = notification.schedule;
   let scheduleDate = schedule?.at;
@@ -63,12 +66,13 @@ test("Day is a Friday", () => {
   expect(scheduleDate?.getHours()).toBe(19);
 });
 
-test("Day is a Friday, fires next week too", () => {
+test("Weekly day-before notifications when today  is a Friday, fires next week too", () => {
   var TEST_DATE = new Date("June 18, 2021 19:01:00");
+  // Not the following Tuesday but the one after that
   let date = new Date("June 29, 2021 19:00:00");
   let notification = weeklySessionDayBefore(2);
 
-  expect(notification.id).toBe(10002);
+  expect(notification.id).toBe(20002);
   expect(notification.title).toBe("Therapy Reminder");
   let schedule = notification.schedule;
   let scheduleDate = schedule?.at;
@@ -76,4 +80,52 @@ test("Day is a Friday, fires next week too", () => {
   expect(scheduleDate?.getMonth()).toBe(5);
   expect(scheduleDate?.getFullYear()).toBe(2021);
   expect(scheduleDate?.getHours()).toBe(19);
+});
+
+test("Weekly HOUR-before notifications when today is a Wednesday day, before 18:00", () => {
+  var TEST_DATE = new Date("June 23, 2021 16:00:00");
+  let date = new Date("June 23, 2021 18:00:00");
+  let notification = weeklySessionNotification(
+    1,
+    3,
+    18,
+    30000,
+    "Your Kalda Group Session starts in 1 hour at 7pm"
+  );
+
+  expect(notification.id).toBe(30001);
+  expect(notification.title).toBe("Therapy Reminder");
+  expect(notification.body).toBe(
+    "Your Kalda Group Session starts in 1 hour at 7pm"
+  );
+  let schedule = notification.schedule;
+  let scheduleDate = schedule?.at;
+  expect(scheduleDate?.getDay()).toBe(3);
+  expect(scheduleDate?.getMonth()).toBe(5);
+  expect(scheduleDate?.getFullYear()).toBe(2021);
+  expect(scheduleDate?.getHours()).toBe(18);
+});
+
+test("Weekly HOUR-before notifications when today is a Wednesday day, AFTER 18:00", () => {
+  var TEST_DATE = new Date("June 16, 2021 18:10:00");
+  let date = new Date("June 23, 2021 18:00:00");
+  let notification = weeklySessionNotification(
+    1,
+    3,
+    18,
+    30000,
+    "Your Kalda Group Session starts in 1 hour at 7pm"
+  );
+
+  expect(notification.id).toBe(30001);
+  expect(notification.title).toBe("Therapy Reminder");
+  expect(notification.body).toBe(
+    "Your Kalda Group Session starts in 1 hour at 7pm"
+  );
+  let schedule = notification.schedule;
+  let scheduleDate = schedule?.at;
+  expect(scheduleDate?.getDay()).toBe(3);
+  expect(scheduleDate?.getMonth()).toBe(5);
+  expect(scheduleDate?.getFullYear()).toBe(2021);
+  expect(scheduleDate?.getHours()).toBe(18);
 });
