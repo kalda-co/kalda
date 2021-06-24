66f65da44ce79e0dd806aeb3c8635a1dc9a0033f
Al Dee <amyalicedee@gmail.com>
2021-06-24 06:15:51 +0100

    Add weekly local notifications for therapy and tests

--- spa/src/__tests__/local-notifications.test.ts
+++ spa/src/__tests__/local-notifications.test.ts
@@ -1,15 +1,24 @@
 import { LocalNotifications } from "@capacitor/local-notifications";
-import {
-  weeklySessionDayBefore,
-  weeklySessionNotification,
-} from "../local-notification";
+import { weeklySessionNotification } from "../local-notification";
 import type { LocalNotificationSchema } from "@capacitor/local-notifications";
 import { date } from "../backend/decode";
+
+const DAY_BEFORE_NOTIFICATION_DAY = 2; // Tuesday
+const DAY_BEFORE_NOTIFICATION_TIME = 19; // 19:00pm
+const DAY_BEFORE_NOTIFICATION_FIRST_ID = 20000;
+const DAY_BEFORE_NOTIFICATION_MESSAGE_BODY =
+  "Your kalda Group Session is tomorrow at 7pm.";
 
 test("Weekly day-before notifications when today is a monday", () => {
   var TEST_DATE = new Date("June 21, 2021 18:00:00");
   let date = new Date("June 22, 2021 19:00:00");
-  let notification = weeklySessionDayBefore(1);
+  let notification = weeklySessionNotification(
+    1,
+    DAY_BEFORE_NOTIFICATION_DAY,
+    DAY_BEFORE_NOTIFICATION_TIME,
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+  );
 
   expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
@@ -24,7 +33,13 @@ test("Weekly day-before notifications when today is a monday", () => {
 test("Weekly day-before notifications when today is a Tuesday, before 19:00", () => {
   var TEST_DATE = new Date("June 22, 2021 18:00:00");
   let date = new Date("June 22, 2021 19:00:00");
-  let notification = weeklySessionDayBefore(1);
+  let notification = weeklySessionNotification(
+    1,
+    DAY_BEFORE_NOTIFICATION_DAY,
+    DAY_BEFORE_NOTIFICATION_TIME,
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+  );
 
   expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
@@ -39,7 +54,13 @@ test("Weekly day-before notifications when today is a Tuesday, before 19:00", ()
 test("Weekly day-before notifications when today  is a Tuesday, after 19:00", () => {
   var TEST_DATE = new Date("June 15, 2021 19:01:00");
   let date = new Date("June 22, 2021 19:00:00");
-  let notification = weeklySessionDayBefore(1);
+  let notification = weeklySessionNotification(
+    1,
+    DAY_BEFORE_NOTIFICATION_DAY,
+    DAY_BEFORE_NOTIFICATION_TIME,
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+  );
 
   expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
@@ -54,7 +75,13 @@ test("Weekly day-before notifications when today  is a Tuesday, after 19:00", ()
 test("Weekly day-before notifications when today  is a Friday", () => {
   var TEST_DATE = new Date("June 18, 2021 19:01:00");
   let date = new Date("June 22, 2021 19:00:00");
-  let notification = weeklySessionDayBefore(1);
+  let notification = weeklySessionNotification(
+    1,
+    DAY_BEFORE_NOTIFICATION_DAY,
+    DAY_BEFORE_NOTIFICATION_TIME,
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+  );
 
   expect(notification.id).toBe(20001);
   expect(notification.title).toBe("Therapy Reminder");
@@ -70,7 +97,13 @@ test("Weekly day-before notifications when today  is a Friday, fires next week t
   var TEST_DATE = new Date("June 18, 2021 19:01:00");
   // Not the following Tuesday but the one after that
   let date = new Date("June 29, 2021 19:00:00");
-  let notification = weeklySessionDayBefore(2);
+  let notification = weeklySessionNotification(
+    2,
+    DAY_BEFORE_NOTIFICATION_DAY,
+    DAY_BEFORE_NOTIFICATION_TIME,
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+  );
 
   expect(notification.id).toBe(20002);
   expect(notification.title).toBe("Therapy Reminder");
@@ -82,15 +115,21 @@ test("Weekly day-before notifications when today  is a Friday, fires next week t
   expect(scheduleDate?.getHours()).toBe(19);
 });
 
+const HOUR_BEFORE_NOTIFICATION_DAY = 3; // Wednesday
+const HOUR_BEFORE_NOTIFICATION_TIME = 18; // 18:00pm
+const HOUR_BEFORE_NOTIFICATION_FIRST_ID = 30000;
+const HOUR_BEFORE_NOTIFICATION_MESSAGE_BODY =
+  "Your Kalda Group Session starts in 1 hour at 7pm";
+
 test("Weekly HOUR-before notifications when today is a Wednesday day, before 18:00", () => {
   var TEST_DATE = new Date("June 23, 2021 16:00:00");
   let date = new Date("June 23, 2021 18:00:00");
   let notification = weeklySessionNotification(
     1,
-    3,
-    18,
-    30000,
-    "Your Kalda Group Session starts in 1 hour at 7pm"
+    HOUR_BEFORE_NOTIFICATION_DAY,
+    HOUR_BEFORE_NOTIFICATION_TIME,
+    HOUR_BEFORE_NOTIFICATION_FIRST_ID,
+    HOUR_BEFORE_NOTIFICATION_MESSAGE_BODY
   );
 
   expect(notification.id).toBe(30001);
@@ -111,10 +150,10 @@ test("Weekly HOUR-before notifications when today is a Wednesday day, AFTER 18:0
   let date = new Date("June 23, 2021 18:00:00");
   let notification = weeklySessionNotification(
     1,
-    3,
-    18,
-    30000,
-    "Your Kalda Group Session starts in 1 hour at 7pm"
+    HOUR_BEFORE_NOTIFICATION_DAY,
+    HOUR_BEFORE_NOTIFICATION_TIME,
+    HOUR_BEFORE_NOTIFICATION_FIRST_ID,
+    HOUR_BEFORE_NOTIFICATION_MESSAGE_BODY
   );
 
   expect(notification.id).toBe(30001);
--- spa/src/local-notification.ts
+++ spa/src/local-notification.ts
@@ -33,64 +33,6 @@ function dailyReflection(index: number): LocalNotificationSchema {
   };
 }
 
-// Date.getDay() returns integer where Sunday == 0
-const WEEKLY_SESSION_REMINDER_DAY = 2; // Tuesday
-const HOURLY_SESSION_REMINDER_DAY = 3; // Wednesday
-const WEEKLY_SESSION_TIME_HOURS = 19; // 19:00pm
-const HOURLY_SESSION_TIME_PROMPT = 18; // 18:00pm
-const WEEKLY_SESSION_FIRST_ID = 20000;
-const HOURLY_SESSION_FIRST_ID = 30000;
-const SESSION_NOTIFICATIONS_TO_SCHEDULE = 3; //up to 3 weeks
-const SESSION_HOUR_BODY = "Your Kalda Group Session starts in 1 hour at 7pm";
-const SESSION_WEEK_BODY = "Your kalda Group Session is tomorrow at 7pm.";
-
-var TEST_DATE: Date;
-
-export function weeklySessionDayBefore(index: number): LocalNotificationSchema {
-  let date = TEST_DATE || new Date();
-  let afterReminderTime = date.getHours() >= WEEKLY_SESSION_TIME_HOURS;
-  let effectiveDayOfWeek = date.getDay() + (afterReminderTime ? 1 : 0);
-  // For example if day of week is monday, getDay returns 1.
-  // 7 - (1 - 2) is 7 - -1 is 8
-  // 8 mod 7 is 1 so on monday reminderInDays returns 1, reminding on Tuesday
-  // if today is thursday, effectiveDayOfWeek is 4, reminderInDays returns 5 (Tuesday)
-  // if today is tuesday before 19:00 effective day of week is 2. reminderInDays (7 - (2 - 2)) mod 7, returns 0
-  let reminderInDays =
-    (7 - (effectiveDayOfWeek - WEEKLY_SESSION_REMINDER_DAY)) % 7;
-  date.setDate(date.getDate() + reminderInDays);
-  date.setHours(WEEKLY_SESSION_TIME_HOURS);
-  date.setMinutes(0);
-  return {
-    id: WEEKLY_SESSION_FIRST_ID + index,
-    title: "Therapy Reminder",
-    body: "Your Kalda Group Session is tomorrow at 7pm",
-    schedule: { at: date },
-  };
-}
-
-export function weeklySessionNotification(
-  index: number,
-  notificationDay: number,
-  notificationTime: number,
-  notificationFirstID: number,
-  notificationBody: string
-): LocalNotificationSchema {
-  let date = TEST_DATE || new Date();
-  let afterReminderTime = date.getHours() >= notificationTime;
-  let effectiveDayOfWeek = date.getDay() + (afterReminderTime ? 1 : 0);
-  let reminderInDays = (7 - (effectiveDayOfWeek - notificationDay)) % 7;
-  date.setDate(date.getDate() + reminderInDays);
-  date.setHours(notificationTime);
-  date.setMinutes(0);
-  return {
-    id: notificationFirstID + index,
-    title: "Therapy Reminder",
-    // body: "Your Kalda Group Session starts in 1 hour at 7pm",
-    body: notificationBody,
-    schedule: { at: date },
-  };
-}
-
 export async function cancelDailyReflectionNotifications(): Promise<void> {
   let notifications = range(DAILY_REFLECTION_DAYS_TO_SCHEDULE).map((index) => ({
     id: DAILY_REFLECTION_FIRST_ID + index,
@@ -124,6 +66,66 @@ async function schedule(
   }
 }
 
+// Date.getDay() returns integer where Sunday == 0
+const DAY_BEFORE_NOTIFICATION_DAY = 2; // Tuesday
+const DAY_BEFORE_NOTIFICATION_TIME = 19; // 19:00pm
+const DAY_BEFORE_NOTIFICATION_FIRST_ID = 20000;
+const DAY_BEFORE_NOTIFICATION_MESSAGE_BODY =
+  "Your kalda Group Session is tomorrow at 7pm.";
+const HOUR_BEFORE_NOTIFICATION_DAY = 3; // Wednesday
+const HOUR_BEFORE_NOTIFICATION_TIME = 18; // 18:00pm
+const HOUR_BEFORE_NOTIFICATION_FIRST_ID = 30000;
+const HOUR_BEFORE_NOTIFICATION_MESSAGE_BODY =
+  "Your Kalda Group Session starts in 1 hour at 7pm";
+
+const NOTIFICATIONS_TO_SCHEDULE = 3; //up to 3 weeks
+var TEST_DATE: Date;
+
+// export function weeklySessionDayBefore(index: number): LocalNotificationSchema {
+//   let date = TEST_DATE || new Date();
+//   let afterReminderTime = date.getHours() >= DAY_BEFORE_NOTIFICATION_TIME;
+//   let effectiveDayOfWeek = date.getDay() + (afterReminderTime ? 1 : 0);
+//   // For example if day of week is monday, getDay returns 1.
+//   // 7 - (1 - 2) is 7 - -1 is 8
+//   // 8 mod 7 is 1 so on monday reminderInDays returns 1, reminding on Tuesday
+//   // if today is thursday, effectiveDayOfWeek is 4, reminderInDays returns 5 (Tuesday)
+//   // if today is tuesday before 19:00 effective day of week is 2. reminderInDays (7 - (2 - 2)) mod 7, returns 0
+//   let reminderInDays =
+//     (7 - (effectiveDayOfWeek - DAY_BEFORE_NOTIFICATION_DAY)) % 7;
+//   date.setDate(date.getDate() + reminderInDays);
+//   date.setHours(DAY_BEFORE_NOTIFICATION_TIME);
+//   date.setMinutes(0);
+//   return {
+//     id: DAY_BEFORE_NOTIFICATION_FIRST_ID + index,
+//     title: "Therapy Reminder",
+//     body: "Your Kalda Group Session is tomorrow at 7pm",
+//     schedule: { at: date },
+//   };
+// }
+
+export function weeklySessionNotification(
+  index: number,
+  notificationDay: number,
+  notificationTime: number,
+  notificationFirstID: number,
+  notificationBody: string
+): LocalNotificationSchema {
+  let date = TEST_DATE || new Date();
+  let afterReminderTime = date.getHours() >= notificationTime;
+  let effectiveDayOfWeek = date.getDay() + (afterReminderTime ? 1 : 0);
+  let reminderInDays = (7 - (effectiveDayOfWeek - notificationDay)) % 7;
+  date.setDate(date.getDate() + reminderInDays);
+  date.setHours(notificationTime);
+  date.setMinutes(0);
+  return {
+    id: notificationFirstID + index,
+    title: "Therapy Reminder",
+    // body: "Your Kalda Group Session starts in 1 hour at 7pm",
+    body: notificationBody,
+    schedule: { at: date },
+  };
+}
+
 export async function cancelNotifications(
   firstNotificationID: number,
   numberNotificationsScheduled: number
@@ -136,26 +138,32 @@ export async function cancelNotifications(
 
 export async function scheduleTherapyNotifications(): Promise<void> {
   await cancelNotifications(
-    WEEKLY_SESSION_FIRST_ID,
-    SESSION_NOTIFICATIONS_TO_SCHEDULE
+    DAY_BEFORE_NOTIFICATION_FIRST_ID,
+    NOTIFICATIONS_TO_SCHEDULE
   );
   await cancelNotifications(
-    HOURLY_SESSION_FIRST_ID,
-    SESSION_NOTIFICATIONS_TO_SCHEDULE
+    HOUR_BEFORE_NOTIFICATION_FIRST_ID,
+    NOTIFICATIONS_TO_SCHEDULE
   );
   await LocalNotifications.schedule({
-    notifications: range(SESSION_NOTIFICATIONS_TO_SCHEDULE).map(
-      weeklySessionDayBefore
+    notifications: range(NOTIFICATIONS_TO_SCHEDULE).map((x) =>
+      weeklySessionNotification(
+        x,
+        DAY_BEFORE_NOTIFICATION_FIRST_ID,
+        DAY_BEFORE_NOTIFICATION_DAY,
+        DAY_BEFORE_NOTIFICATION_TIME,
+        DAY_BEFORE_NOTIFICATION_MESSAGE_BODY
+      )
     ),
   });
   await LocalNotifications.schedule({
-    notifications: range(SESSION_NOTIFICATIONS_TO_SCHEDULE).map((x) =>
+    notifications: range(NOTIFICATIONS_TO_SCHEDULE).map((x) =>
       weeklySessionNotification(
         x,
-        HOURLY_SESSION_REMINDER_DAY,
-        HOURLY_SESSION_TIME_PROMPT,
-        HOURLY_SESSION_FIRST_ID,
-        SESSION_HOUR_BODY
+        HOUR_BEFORE_NOTIFICATION_DAY,
+        HOUR_BEFORE_NOTIFICATION_TIME,
+        HOUR_BEFORE_NOTIFICATION_FIRST_ID,
+        HOUR_BEFORE_NOTIFICATION_MESSAGE_BODY
       )
     ),
   });
