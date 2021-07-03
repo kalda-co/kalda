90d6789e681dfd0338cc8e0ea4c42088d5cd00ff
Louis Pilfold <louis@lpil.uk>
2021-07-03 14:51:29 +0100

    wip

--- lib/kalda/payments.ex
+++ lib/kalda/payments.ex
@@ -3,7 +3,25 @@ defmodule Kalda.Payments do
   The Payments context.
   """
 
-  @spec create_stripe_customer(String.t()) :: {:ok}
-  def create_stripe_customer(email) do
+  alias Kalda.Payments.StripeCustomer
+  alias Kalda.Accounts.User
+  alias Stripe, as: StripeLibrary
+
+  @doc """
+  Create a new stripe customer using the Stripe API and insert it into our database.
+  """
+  @spec create_stripe_customer(User.t()) ::
+          {:ok, StripeCustomer.t()} | {:error, StripeLibrary.Error.t()}
+  def create_stripe_customer(user = %User{}) do
+    with {:ok, customer} <- StripeLibrary.Customer.create(%{email: user.email}) do
+      {:ok, insert_stripe_customer!(user, customer)}
+    end
+  end
+
+  @spec insert_stripe_customer!(User.t(), StripeLibrary.Customer.t()) :: StripeCustomer.t()
+  defp insert_stripe_customer!(user = %User{}, customer = %Stripe.Customer{}) do
+    %StripeCustomer{user: user, stripe_id: customer.id}
+    |> StripeCustomer.changeset(%{})
+    |> Kalda.Repo.insert!()
   end
 end
