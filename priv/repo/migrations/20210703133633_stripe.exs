3aeeef4f97cc3c4da3144a32b36c9f3ef7f7d166
Louis Pilfold <louis@lpil.uk>
2021-07-14 19:15:22 +0100

    wip

--- priv/repo/migrations/20210703133633_stripe.exs
+++
@@ -1,9 +0,0 @@
-defmodule Kalda.Repo.Migrations.Stripe do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :stripe_customer_id, :string, null: true
-    end
-  end
-end
