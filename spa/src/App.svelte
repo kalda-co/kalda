ef4efc920c355973c7938e1bfd1ffc8840765a40
Al Dee <amyalicedee@gmail.com>
2021-08-26 17:09:13 +0100

    Fix incomplete routes (links) from SPA to webapp

--- spa/src/App.svelte
+++ spa/src/App.svelte
@@ -145,12 +145,13 @@
     </form>
 
     <p class="forgot-password">
-      <a href="/users/reset-password">Forgot your password?</a>
+      <!-- TODO: these links do not work in dev or staging because base_url is not implemented for the SPA. -->
+      <a href="https://kalda.co/users/reset-password">Forgot your password?</a>
     </p>
     <div>
       <p>
         Haven't created an account yet? <span class="forgot-password">
-          <a href="/users/register">Register here.</a>
+          <a href="https://kalda.co/users/register">Register here.</a>
         </span>
       </p>
     </div>
