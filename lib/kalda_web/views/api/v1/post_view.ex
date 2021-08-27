15af12ae026d81c646be3d0cbd1e35b8b5161daa
Al Dee <amyalicedee@gmail.com>
2021-08-27 16:37:37 +0100

    For the single post notification view, comments ordered by notification-comment first

--- lib/kalda_web/views/api/v1/post_view.ex
+++ lib/kalda_web/views/api/v1/post_view.ex
@@ -3,16 +3,16 @@ defmodule KaldaWeb.Api.V1.PostView do
   alias KaldaWeb.Api.V1.UserView
   alias KaldaWeb.Api.V1.CommentView
 
-  def render("show.json", %{post: post}) do
-    render_post(post)
+  def render("show.json", %{post: post, ordered_comments: ordered_comments}) do
+    render_post(post, ordered_comments)
   end
 
-  defp render_post(post) do
+  defp render_post(post, ordered_comments) do
     %{
       id: post.id,
       author: UserView.render_author(post.author),
       content: post.content,
-      comments: Enum.map(post.comments, &CommentView.render_comment/1),
+      comments: Enum.map(ordered_comments, &CommentView.render_comment/1),
       published_at: post.published_at,
       forum: post.forum
     }
