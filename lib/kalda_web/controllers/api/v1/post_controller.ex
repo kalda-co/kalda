386cc2bb7683d56d9e1e45617f6329594797492f
Al Dee <amyalicedee@gmail.com>
2021-08-27 14:23:48 +0100

    Orderes comments on post, so comment for notification is first

--- lib/kalda_web/controllers/api/v1/post_controller.ex
+++ lib/kalda_web/controllers/api/v1/post_controller.ex
@@ -5,10 +5,27 @@ defmodule KaldaWeb.Api.V1.PostController do
 
   def show(conn, %{"id" => id}) do
     post = Forums.get_post_order_preloads!(id)
+    comments = post.comments
+
+    comment_id = "abc"
+
+    original_comment =
+      Enum.filter(comments, fn
+        comment -> comment.id == comment_id
+      end)
+
+    other_comments =
+      Enum.filter(comments, fn
+        comment -> comment.id != comment_id
+      end)
+
+    ordered_comments = List.flatten([original_comment, other_comments])
+
+    IO.inspect(original_comment)
 
     conn
     |> put_status(200)
-    |> render("show.json", post: post)
+    |> render("show.json", post: post, ordered_comments: ordered_comments)
     |> KaldaWeb.Api.V1.handle_error(conn)
   end
 end
